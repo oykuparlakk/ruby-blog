1db906ff50d477cace4755010699eb23f9e58330
öykü parlak <56317041+oykuparlakk@users.noreply.github.com>
2024-10-18 14:56:44 +0300

    deleted unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  delete "articles/:id", to:"articles#destroy"
 end
