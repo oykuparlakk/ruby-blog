e39f2f6252ea7424c0db46dbed6c54b942479ddf
öykü parlak <56317041+oykuparlakk@users.noreply.github.com>
2024-11-19 13:22:21 +0300

    feature:tailwindcss added

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+gem "tailwindcss-rails"
--- config/puma.rb
+++ config/puma.rb
@@ -21,6 +21,7 @@
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+plugin :tailwindcss if ENV.fetch("RAILS_ENV", "development") == "development"
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
