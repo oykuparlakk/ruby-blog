6524106d5cd1d41ccd8d981956d9151779ab5b2b
öykü parlak <56317041+oykuparlakk@users.noreply.github.com>
2024-11-21 22:30:12 +0300

    feat: pagination added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  scope "(:locale)", locale: /en|tr/ do
+    root "articles#index"
 
-  resources :articles do
-    resources :comments, only: [ :create, :destroy ]
+    resources :articles do
+      resources :comments, only: [ :create, :destroy ]
 
-    collection do
-      get "search", to: "articles#search"
+      collection do
+        get "search", to: "articles#search"
+      end
     end
   end
 end
